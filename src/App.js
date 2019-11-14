158e34c85f2baadaee65709a181b1cd600e6dfcc
Jackie-Tran <jtran2009a@gmail.com>
2019-11-13 19:04:51 -0500

    styled compenents on page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import Grid from './components/Grid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    grid: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    ]
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Sudoku Solver</h1>
+          <h4>Backtracking Algorthim</h4>
+          <a href="#solver">
+            <div className="btn btn-one">
+              <span>TRY NOW</span>
+            </div>
+          </a>
+
+        </header>
+
+        {/* Section Break */}
+        
+
+        <div className="solverContainer" id='solver'>
+          <h2>Input data in the grid below.</h2>
+          <Grid />
+          <a>
+            <div className="btn btn-five">
+              <span>SOLVE</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
