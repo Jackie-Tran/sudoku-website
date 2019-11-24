f5bdfd4bc4aa043fd2b34eb48bccc80fedd4087f
Jackie <jtran2009a@gmail.com>
2019-11-23 21:39:55 -0500

    Updates

---
+++ precache-manifest.b837d320f85bf5ea53686ed72f048952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120a6fd4ae58a003c705561dcaba40ba",
+    "url": "/sudoku-website/index.html"
+  },
+  {
+    "revision": "b7ba081d9a1985485ed0",
+    "url": "/sudoku-website/static/css/main.9644d35e.chunk.css"
+  },
+  {
+    "revision": "4490eb7d4fc062b49b77",
+    "url": "/sudoku-website/static/js/2.fedbdfd8.chunk.js"
+  },
+  {
+    "revision": "b7ba081d9a1985485ed0",
+    "url": "/sudoku-website/static/js/main.a254a23b.chunk.js"
+  },
+  {
+    "revision": "1d932e665f5d879066f0",
+    "url": "/sudoku-website/static/js/runtime-main.b1b9cf62.js"
+  }
+]);
